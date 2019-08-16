ae2e662c0c92282ebc87531380049b82c523fd98
Patrycja Petryk <patrycja.petryk@gmail.com>
2019-08-16 22:22:59 +0200

    feat: water animation

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,9 @@ const value = document.querySelector('.counter__value--js');
 const key = new Date().toISOString().slice(0, 10);
 const history = document.querySelector('.history--js');
 const list = document.querySelector('.history__list--js');
+const glassWater = document.querySelector('.glass__water-svg--js');
+const glassWaterTop = document.querySelector('.glass__water1-svg--js');
+const glassBubbles = document.querySelector('.glass__bubbles-svg--js');
 
 if (!localStorage.getItem(key)) {
   localStorage.setItem(key, 0)
@@ -31,9 +34,28 @@ if (!localStorage.getItem(key)) {
   value.innerHTML = localStorage.getItem(key);
 };
 
+const waterPosition = () => {
+  if (localStorage.getItem(key) <= 8) {
+    glassWater.style.clipPath = `inset(${84 - (localStorage.getItem(key) * 8)}% 0 0 0 round 0)`;
+    glassWaterTop.style.transform = `translateY(${62 - (localStorage.getItem(key) * 8)}%) scaleX(${.82 + localStorage.getItem(key)/40})`;
+    if (localStorage.getItem(key) > 7) {
+      glassBubbles.style.opacity = 1;
+    } else {
+      glassBubbles.style.opacity = 0;
+    }
+  } else {
+    glassWater.style.clipPath = 'inset(0 0 0 0 round 0)';
+    glassWaterTop.style.transform = 'translateY(-2%) scaleX(1.02)';
+    glassBubbles.style.opacity = 1;
+  }
+};
+
+waterPosition();
+
 buttonAdd.addEventListener('click', (e) => {
   localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
   value.innerHTML = localStorage.getItem(key);
+  waterPosition();
 });
 
 buttonRemove.addEventListener('click', (e) => {
@@ -42,6 +64,7 @@ buttonRemove.addEventListener('click', (e) => {
     localStorage.setItem(key, localStorage.getItem(key) - 1);
     value.innerHTML = localStorage.getItem(key);
   }
+  waterPosition();
 });
 
 buttonHistory.addEventListener('click', (e) => {
@@ -66,7 +89,7 @@ buttonHistory.addEventListener('click', (e) => {
       copy = 'szklanki';
     }
     if (key[0] == 2) {
-      list.innerHTML += `<li class="history__item">${key} / <span class="history__span">${value} ${copy}</span></li>`;
+      list.innerHTML += `<li class="history__item">${key} / <span class="history__span">${value} ${copy} ${(value>7)?'👏'}</span></li>`;
     };
   }
 });
